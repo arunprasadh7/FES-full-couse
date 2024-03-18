2ffc9c717347616d6da9d3b0a4fe4bf7a090bba9
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-03-18 17:28:30 +0530

    React Basics with Webpack

---
+++ Week 4 - React/react-crash-course/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <h1>My TODO List</h1>
+      <div className="">
+        <h2>Finished Frontend Simplified</h2>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
